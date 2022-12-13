7e98733563fc4218579a17ec3f89a7d7fa129054
pluswillseo <115020939+pluswillseo@users.noreply.github.com>
2022-12-13 18:00:24 +0000

    Update categorise-st.py

--- categorise-st.py
+++ categorise-st.py
@@ -53,7 +53,7 @@ st.sidebar.title("Filters")
 selected_categories = st.sidebar.multiselect("Select categories to filter by:", options=list(products.keys()))
 
 # Filter the table by the selected categories
-filtered_df = df[np.isin(df["Categories"], selected_categories)]
+filtered_df = df[np.isin(selected_categories, df["Categories"])]
 
 # Display the filtered DataFrame
 st.table(filtered_df)
