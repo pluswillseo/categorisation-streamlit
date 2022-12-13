3a6f835b6f676073ba9dcb0eaae687eb4bb4c4dc
pluswillseo <115020939+pluswillseo@users.noreply.github.com>
2022-12-13 21:46:52 +0000

    Update categorise-st.py

--- categorise-st.py
+++ categorise-st.py
@@ -43,9 +43,6 @@ df = pd.DataFrame(output)
 
 df.rename(columns = {0:'Keywords', 1:'Categories'}, inplace = True)
 
-csv = df.to_csv(index=False)
-st.download_button('Download Table as CSV', csv, file_name = 'output.csv', mime='text/csv')
-
 # Add a sidebar to the app
 st.sidebar.title("Filter Categories")
 
