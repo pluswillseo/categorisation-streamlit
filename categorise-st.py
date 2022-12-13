d76a2dfab18f89ae1e6cb898248d63d553311262
pluswillseo <115020939+pluswillseo@users.noreply.github.com>
2022-12-13 13:10:58 +0000

    Update categorise-st.py

--- categorise-st.py
+++ categorise-st.py
@@ -14,7 +14,7 @@ while True:
     category = st.text_input("Enter category name (leave empty to finish):", key="category_{}".format(category_count))
     if not category:
         break
-    keywords = st.text_input("Enter keywords for category '{}', separated by commas:".format(category), key="keywords_{}".format(category_count))
+    keywords = st.text_input("Enter keywords for category '{}', separated by commas. This can also include regex:".format(category), key="keywords_{}".format(category_count))
     keywords = [k.strip() for k in keywords.split(",")]
     products[category] = keywords
 
