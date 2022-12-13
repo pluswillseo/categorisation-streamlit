2c5327c36a02c8a370c526adccabbbf0be287378
pluswillseo <115020939+pluswillseo@users.noreply.github.com>
2022-12-13 13:41:03 +0000

    Update categorise-st.py

--- categorise-st.py
+++ categorise-st.py
@@ -40,13 +40,6 @@ import pandas as pd
 
 df = pd.Dataframe(output)
 
-@st.cache
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
-
-csv = convert_df(df)
-
-st.download_button(label="Download data as CSV", data=csv, file_name='large_df.csv', mime='text/csv')
+st.dataframe(df)
 
 st.table(output)
