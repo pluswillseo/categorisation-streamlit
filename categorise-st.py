ff6b64bec5ace795cf30d4ff9c69045b5c03a7e9
pluswillseo <115020939+pluswillseo@users.noreply.github.com>
2022-12-14 15:11:43 +0000

    Update categorise-st.py

--- categorise-st.py
+++ categorise-st.py
@@ -7,22 +7,22 @@ import numpy as np
 st.title("Keyword Categoriser")
 
 # Define a dictionary of Samsung products and their relevant keywords
-st.header("Categories and Keywords")
+st.header("Categories")
 st.write("Add the categories you would like to use, along with the keywords and regex that identify each. Once you've completed all your categories, leave the last one blank and move on to adding your keywords.")
 products = {}
 category_count = 0
 while True:
     category_count += 1
-    category = st.text_input("Enter category name (leave empty to finish):", key="category_{}".format(category_count))
+    category = st.text_input("Enter category name - if you don't need another, leave it blank:", key="category_{}".format(category_count))
     if not category:
         break
-    keywords = st.text_input("Enter keywords for category '{}', separated by commas. This can also include regex:".format(category), key="keywords_{}".format(category_count))
+    keywords = st.text_input("Add the identifiers for the category '{}', separated by commas. This can also use Regex:".format(category), key="keywords_{}".format(category_count))
     keywords = [k.strip() for k in keywords.split(",")]
     products[category] = keywords
 
 # Read the keywords from the keywords.csv file
 st.header("Keywords")
-keywords = st.text_area("Enter keywords, separated by newlines:", key="input_keywords")
+keywords = st.text_area("Enter keywords, separated by a new line for each:", key="input_keywords")
 keywords = keywords.split("\n")
 
 # Loop through each keyword and write its corresponding category to the output.csv file
@@ -36,7 +36,7 @@ for keyword in keywords:
                 break
     output.append([keyword, ", ".join(categories)])
 
-st.header("Output")
+st.header("Full data table")
 
 import pandas as pd
 
@@ -45,7 +45,7 @@ df = pd.DataFrame(output)
 df.rename(columns = {0:'Keywords', 1:'Categories'}, inplace = True)
 
 # Add a sidebar to the app
-st.sidebar.title("Filter Categories")
+st.sidebar.title("Filter categories")
 
 # Add a filter to the sidebar that allows users to select multiple categories
 selected_categories = st.sidebar.multiselect("Select categories to filter by:", options=list(products.keys()))
